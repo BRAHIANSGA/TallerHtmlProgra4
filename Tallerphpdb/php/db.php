c08b55188778a3c29b1d5983c5252892509ff296
BRAHIANSGA <brahian.gil@ucp.edu.co>
2023-11-10 11:13:09 -0500

    CAMBIOS

---
+++ Tallerphpdb/php/db.php
@@ -0,0 +1,14 @@
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "brahiang321";
+$dbname = "tallerphp";
+
+// Crear conexión
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Verificar conexión
+if ($conn->connect_error) {
+  die("Conexión fallida: " . $conn->connect_error);
+}
+?>
